5373b0c4cc88dc0cf58a6d2b8f5da43d2e2cc69f
Skendri <skendripeza0@gmail.com>
2025-05-23 13:13:49 +0200

    if commit

--- src/components/testimonials-props.js
+++ src/components/testimonials-props.js
@@ -1,5 +1,6 @@
-
 import React, { useState } from "react";
+// import {motion, AnimatePresence} from "framer-motion";
+
 const testimonialsProps = [
   {description: `“We’ve used Traveler to lorem ipsum dolor sit amet, consectetur
     adipiscing elit, sed do eiusmod tempor incididunt ut labore et
@@ -19,7 +20,7 @@ const testimonialsProps = [
     },
 ];
 
- function testimonialsCarousel(props) {
+ function TestimonialsCarousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const goNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonialsProps.length);
@@ -31,37 +32,55 @@ const testimonialsProps = [
     console.log("butoni u klikua");
   }
 
+  // const current = testimonialsProps[currentIndex];
+
 
   return (
-    <div className="home-content12">
-              <div className="home-heading12">
-                <span className="home-title">Customer testimonial</span>
-                <p className="home-quote">
-                  {testimonialsProps[currentIndex].description}
-                </p>
-              </div>
-              <div className="home-details">
-                <div className="home-author">
-                  <img
-                    alt="image"
-                    src={testimonialsProps[currentIndex].image}
-                    className="home-avatar"
-                  />
-                  <span className="home-name">{testimonialsProps[currentIndex].name}</span>
-                </div>
-                <div className="home-controls">
-                  <div className="page" onClick={goPrev}>
-                    <span className="home-text23" >&lt;</span>
-                  </div>
-                  <div className="home-next2 page" onClick={goNext}>
-                    <span className="home-text24" >&lt;</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-  )
+  <div className="home-content12">
+    {/* <AnimatePresence mode="wait">
+      <motion.div 
+        key={currentIndex}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}> */}
+    <div className="home-heading12">
+      <span className="home-title">Customer testimonial</span>
+      <p className="home-quote">
+        {testimonialsProps[currentIndex].description}
+      </p>
+    </div>
 
-  // const current = testimonialsProps[currentIndex];
+    <div className="home-details">
+      <div className="home-author">
+        <img
+          alt="image"
+          src={testimonialsProps[currentIndex].image}
+          className="home-avatar"
+        />
+        <span className="home-name">
+          {testimonialsProps[currentIndex].name}
+        </span>
+      </div>
+      
+      
+
+      {/* buttons for moving slider left or right */}
+      <div className="home-controls">
+        <div className="page" onClick={goPrev}>
+          <span className="home-text23">&lt;</span>
+        </div>
+        <div className="home-next2 page" onClick={goNext}>
+          <span className="home-text24">&lt;</span>
+        </div>
+      </div>
+      
+    </div>
+    {/* </motion.div>
+    </AnimatePresence> */}
+  </div>
+  
+);
 }
 
-export default testimonialsCarousel
+export default TestimonialsCarousel
